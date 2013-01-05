98f36f37d11f3f7c42046d71c2054b4be89051a3
RoCry <crysheen@gmail.com>
2013-01-05 16:32:18 +0800

    Initial Commit

---
+++ CustomSegueDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CustomSegueDemo
+//
+//  Created by RoCry on 1/5/13.
+//  Copyright (c) 2013 RoCry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CustomSegueDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CustomSegueDemo
+//
+//  Created by RoCry on 1/5/13.
+//  Copyright (c) 2013 RoCry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
